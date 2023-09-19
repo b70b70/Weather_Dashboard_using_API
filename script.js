3481642eaedfbd88826a824a0c9b4aeb93437e01
Ali <134099889+b70b70@users.noreply.github.com>
2023-09-19 21:50:06 +0100

    I have improved the UI & UX of the website, initial design for an easier interface using Bootstrap and css styling.

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function fetchGeoLocation(city) {
 
         // Use the obtained coordinates (latitude and longitude) as needed
         console.log(`Coordinates for ${city}: Latitude ${latitude}, Longitude ${longitude}`);
-        
+
         // Now, you can proceed to fetch weather data using these coordinates
         fetchWeatherData(city, latitude, longitude);
       } else {
@@ -52,51 +52,55 @@ function fetchWeatherData(city, latitude, longitude) {
 
 // Function to display the current weather and 5-day forecast
 function updateUI(city, data) {
-    // Extract and display current weather information here
-    const temperature = data.list[0].main.temp;
-    const humidity = data.list[0].main.humidity;
-  
-    // Update the UI element with the weather information
-    const currentWeatherSection = document.getElementById('current-weather');
-    currentWeatherSection.innerHTML = `
-      <h2>${city}</h2>
-      <p>Temperature: ${temperature}°C</p>
-      <p>Humidity: ${humidity}%</p>
-      <!-- Add more weather details as needed -->
+  // Extract and display current weather information here
+  const temperature = data.list[0].main.temp;
+  const humidity = data.list[0].main.humidity;
+  const weatherIconCode = data.list[0].weather[0].icon;
+
+  // Update the UI element with the weather information, including a resized icon
+  const currentWeatherSection = document.getElementById('current-weather');
+  currentWeatherSection.innerHTML = `
+    <h2>${city}</h2>
+    <p>Temperature: ${temperature}°C</p>
+    <p>Humidity: ${humidity}%</p>
+    <img src="https://openweathermap.org/img/w/${weatherIconCode}.png" alt="Weather Icon" class="weather-icon"> <!-- Add the "weather-icon" class -->
+    <!-- Add more weather details as needed -->
+  `;
+
+  // Extract and display 5-day forecast information here
+  const forecastSection = document.getElementById('forecast');
+  const forecasts = data.list.slice(1, 6); // Exclude the current day's data
+
+  // Generate HTML for the forecast entries
+  let forecastHtml = '<h2>5-Day Forecast</h2>';
+
+  forecasts.forEach((forecast) => {
+    const date = forecast.dt_txt; // Date and time
+    const temperature = forecast.main.temp;
+    const humidity = forecast.main.humidity;
+    const weatherIconCode = forecast.weather[0].icon; // Get the weather icon code
+
+    // Create an image element for the weather icon
+    const weatherIconUrl = `https://openweathermap.org/img/w/${weatherIconCode}.png`;
+    const weatherIcon = `<img src="${weatherIconUrl}" alt="Weather Icon">`;
+
+    // Create HTML for each forecast entry
+    forecastHtml += `
+      <div class="forecast-entry">
+        <p>Date: ${date}</p>
+        ${weatherIcon} <!-- Display the weather icon -->
+        <p>Temperature: ${temperature}°C</p>
+        <p>Humidity: ${humidity}%</p>
+        <!-- Add more forecast details as needed -->
+      </div>
     `;
-  
-    // Extract and display 5-day forecast information here
-    const forecastSection = document.getElementById('forecast');
-    const forecasts = data.list.slice(1, 6); // Exclude the current day's data
-  
-    // Generate HTML for the forecast entries
-    let forecastHtml = '<h2>5-Day Forecast</h2>';
-  
-    forecasts.forEach((forecast) => {
-      const date = forecast.dt_txt; // Date and time
-      const temperature = forecast.main.temp;
-      const humidity = forecast.main.humidity;
-      const weatherIconCode = forecast.weather[0].icon; // Get the weather icon code
-  
-      // Create an image element for the weather icon
-      const weatherIconUrl = `https://openweathermap.org/img/w/${weatherIconCode}.png`;
-      const weatherIcon = `<img src="${weatherIconUrl}" alt="Weather Icon">`;
-  
-      // Create HTML for each forecast entry
-      forecastHtml += `
-        <div class="forecast-entry">
-          <p>Date: ${date}</p>
-          ${weatherIcon} <!-- Display the weather icon -->
-          <p>Temperature: ${temperature}°C</p>
-          <p>Humidity: ${humidity}%</p>
-          <!-- Add more forecast details as needed -->
-        </div>
-      `;
-    });
-  
-    // Update the UI element with the forecast data
-    forecastSection.innerHTML = forecastHtml;
-  }
+  });
+
+  // Update the UI element with the forecast data
+  forecastSection.innerHTML = forecastHtml;
+}
+
+
 
 // Function to add a city to the search history
 function addToSearchHistory(city) {
@@ -111,23 +115,27 @@ function addToSearchHistory(city) {
 
 // Function to update the search history UI
 function updateSearchHistoryUI() {
-    const historyList = document.getElementById('history-list'); // Updated to 'history-list'
-  
-    // Clear the existing history list
-    historyList.innerHTML = '';
-  
-    // Add each city in the search history as a clickable item
-    searchHistory.forEach((city) => {
-      const historyItem = document.createElement('li'); // Change to 'li' element
-      historyItem.textContent = city;
-      historyItem.addEventListener('click', () => {
-        // When a user clicks on a city in the history, fetch weather data again
-        fetchGeoLocation(city);
-      });
-  
-      historyList.appendChild(historyItem);
+  const historyList = document.getElementById('history-list'); // Updated to 'history-list'
+
+  // Clear the existing history list
+  historyList.innerHTML = '';
+
+  // Add each city in the search history as a clickable item (button)
+  searchHistory.forEach((city) => {
+    const historyItem = document.createElement('li'); // Change to 'li' element
+    const button = document.createElement('button'); // Create a button element
+    button.textContent = city;
+    button.addEventListener('click', () => {
+      // When a user clicks on a city in the history, fetch weather data again
+      fetchGeoLocation(city);
     });
-  }
+
+    // Append the button to the list item
+    historyItem.appendChild(button);
+
+    historyList.appendChild(historyItem);
+  });
+}
 
 // Event listener for the search form submission
 document.getElementById('search-form').addEventListener('submit', (event) => {
@@ -136,7 +144,4 @@ document.getElementById('search-form').addEventListener('submit', (event) => {
 
   // Call the function to fetch geo location for the city
   fetchGeoLocation(city);
-
-  
 });
-
